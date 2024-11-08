418aea373ee9a0d96fc2ab6bdb6e06c24f6caa1d
petrilya06 <petrilya06@gmail.com>
2024-11-08 21:29:35 +0300

    add inlinekeyboard

--- bot/bot.go
+++ bot/bot.go
@@ -1,9 +1,16 @@
 package bot
 
 import (
+	"log"
 	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+var criterias = tgbotapi.NewInlineKeyboardMarkup(
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData("Изменить критерии", "CHANGE_CRITERIA"),
+	),
 )
 
 func RunBot() {
@@ -20,10 +27,26 @@ func RunBot() {
 
 	for update := range updates {
 		if update.Message != nil {
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-			msg.ReplyToMessageID = update.Message.MessageID
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
 
-			bot.Send(msg)
+			switch update.Message.Text {
+			case "/start":
+				msg.Text = "Бу! Испугался? Не бойся, я друг.\nВключить все критерии или нет?"
+				msg.ReplyMarkup = criterias
+			}
+			if _, err := bot.Send(msg); err != nil {
+				log.Panic(err)
+			}
+		} else if update.CallbackQuery != nil {
+			callback := tgbotapi.NewCallback(update.CallbackQuery.ID, update.CallbackQuery.Data)
+			if _, err := bot.Request(callback); err != nil {
+				panic(err)
+			}
+
+			msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Data)
+			if _, err := bot.Send(msg); err != nil {
+				panic(err)
+			}
 		}
 	}
 }
